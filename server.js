8166378b6400ab52bf05618a99137d16a03da7ed
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-06-17 22:39:50 -0400

    Intro to Express Part 1

---
+++ server.js
@@ -0,0 +1,19 @@
+// express was installed in node modules so we do not need to give it a file path
+const express = require('express');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+// set up the server so that it returns the same response for any request we use the use method that can take the callback function
+app.use((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
